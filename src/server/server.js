f8a3ef5198ac8d2e8409318116deacd5a1501804
Deisya Dzakiyyah Rahmawati <145454921+deisyadzakiyyah09@users.noreply.github.com>
2024-12-22 23:49:32 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,4 @@
+
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
@@ -6,7 +7,7 @@ const InputError = require('../exceptions/InputError');
  
 (async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT || 3000,
         host: '0.0.0.0',
         routes: {
             cors: {
